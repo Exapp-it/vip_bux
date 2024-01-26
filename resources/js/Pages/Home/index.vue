41b9568dacaf320fc0415b51410d4fda16622cdb
Exapp Company <asa0507790@gmail.com>
2024-01-26 18:20:10 +0400

    Start project

---
+++ resources/js/Pages/Home/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ title: String })
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <h1>Welcome</h1>
+    <p>Hello {{ title }}, welcome to your first Inertia app!</p>
+</template>
