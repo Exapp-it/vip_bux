88147b816fd1b604ee5394012202bb867825553b
Exapp Company <asa0507790@gmail.com>
2024-01-31 11:16:51 +0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+git pull origin production
+php artisan down
+php artisan composer --no-dev --optimize-autoloader
+php artisan migrate --force
+php artisan config:cache
+php artisan view:cache
+php artisan route:cache
+php artisan event:cache
+php artisan up
+
